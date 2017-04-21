958b98fc347f4dc29051ba38758611e80b692b85
Marco Milon <marco.milon@gmail.com>
2017-04-21 13:14:40 -0500

    New version

--- web/src/config/web.php
+++ web/src/config/web.php
@@ -5,6 +5,6 @@ $app = dirname(dirname(__FILE__));
 $params = [
   'api' => 'http://localhost/wasi/api/v1',
   'app' => $app,
-  'public' => '/wasi/web/public_html/',
+  'public' => '',
   'defaultController' => 'DefaultController',
 ];
