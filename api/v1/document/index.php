2f9bf06a6d8b66395d8e6bb291ff98c821528c67
marcomilon <marco.milon@gmail.com>
2017-08-21 21:57:45 -0500

    New version

--- api/v1/document/index.php
+++ api/v1/document/index.php
@@ -5,7 +5,7 @@ include '../../bootstrap.php';
 use Wasi\Api\Io\Document;
 use Wasi\Api\RouterWrapper;
 
-$document = new Set(new Wasi\Api\Io\FileSystem\Document());
+$document = new Document(new Wasi\Api\Io\FileSystem\Document());
 $model = $document->getModel();
 $uri = 'documents';
 $routerWrapper = new RouterWrapper($model, $uri);
