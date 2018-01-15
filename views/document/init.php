10ffaa98c402abba503ad8b0d20c85d7be18e0df
Marco Milon <marco.milon@gmail.com>
2018-01-15 15:01:13 -0500

    Rename view

--- views/document/init.php
+++ views/document/init.php
@@ -1,7 +1,7 @@
 <div class="main">
     <h3 class="main__title">New document</h3>
     <div class="row">
-        <div class="col-md-8">
+        <div class="col-md-4">
             <nav aria-label="breadcrumb" role="navigation">
                 <ol class="breadcrumb">
                     <li class="breadcrumb-item"><a href="index.php?r=form">Home</a></li>
@@ -13,14 +13,14 @@
                 <div class="form-group">
                     <label for="set">Set</label>
                     <select class="form-control" id="set" name="set" required>
-                        <option selected>Choose a set...</option>
+                        <option value="">Choose a set...</option>
                         <?php foreach($models as $model): ?>
                             <option value="<?= $model->id ?>"><?= $model->title ?></option>
                         <?php endforeach; ?>
                     </select>
                 </div>
                 <div class="text-right">
-                    <button type="submit" class="btn btn-primary" role='button'>Continue <i class="fas fa-arrow-right"></i></button>
+                    <button type="submit" class="btn btn-primary btn-sm" role='button'>Continue <i class="fas fa-arrow-right"></i></button>
                 </div>
             </form>
         </div>
