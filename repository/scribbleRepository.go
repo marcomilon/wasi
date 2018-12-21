57850012d7c24afcba3cb5d3fbab74d654bec97f
Marco Milon <marco.milon@gmail.com>
2018-12-20 23:10:37 -0500

    Working ScribbleRepo

--- repository/scribbleRepository.go
+++ repository/scribbleRepository.go
@@ -1,15 +1,19 @@
 package repository
 
 import (
+	"encoding/json"
+	"fmt"
+
 	"github.com/marcomilon/wasi/element"
 	scribble "github.com/nanobox-io/golang-scribble"
 )
 
 type ScribbleRepository struct {
-	db *scribble.Driver
+	dbName string
+	db     *scribble.Driver
 }
 
-func NewScribbleRepository(storegePath string) (ScribbleRepository, error) {
+func NewScribbleRepository(dbname string, storegePath string) (ScribbleRepository, error) {
 
 	var r ScribbleRepository
 
@@ -18,12 +22,21 @@ func NewScribbleRepository(storegePath string) (ScribbleRepository, error) {
 		return r, err
 	}
 
-	r = ScribbleRepository{db}
+	r = ScribbleRepository{dbname, db}
 
 	return r, nil
 }
 
 func (s ScribbleRepository) Store(e element.Identifier) {
+	// fmt.Print("Body: " + e.GetBody())
+	//uuidStr := uuid4.New().String()
+	//e.SetID(uuidStr)
+
+	b, _ := json.Marshal(e)
+	fmt.Println("Json: " + string(b))
+	// if err := s.db.Write(s.dbName, uuidStr, e); err != nil {
+	// }
+
 }
 
 func (s ScribbleRepository) List() []element.Identifier {
--- repository/scribbleRepository_test.go
+++ repository/scribbleRepository_test.go
@@ -3,14 +3,30 @@ package repository
 import (
 	"os"
 	"testing"
+
+	"github.com/marcomilon/wasi/element"
 )
+
+type Example struct{ Name string }
 
 func TestNew(t *testing.T) {
 	storagePath := "../storage/testdb"
 
-	NewScribbleRepository(storagePath)
+	NewScribbleRepository("elements", storagePath)
 
 	if _, err := os.Stat(storagePath); err != nil {
 		t.Error("Expected database storage path: ", storagePath)
+	}
+}
+
+func TestSetID(t *testing.T) {
+	storagePath := "../storage/testdb"
+
+	r, _ := NewScribbleRepository("elements", storagePath)
+	atom := element.NewAtom("Test 1")
+
+	r.Store(atom)
+	if atom.GetID() == "" {
+		t.Error("Expected ID not empty, got ", atom.GetID())
 	}
 }
